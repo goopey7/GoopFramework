5ad32a2641b8887e30781a29ed1ee43206ccc9d8
Sam Collier <sam@samcollier.tech>
2022-02-25 17:31:19 +0000

    Node class

---
+++ Node.cpp
@@ -0,0 +1,100 @@
+//Copyright Sam Collier 2022
+
+//**************************************************************************************
+//Heavily influenced by 
+//Chapter 3 of SFML Game Development by Artur Moreira, Henrik V. Hansson, and Jan Haller
+//**************************************************************************************
+
+#include "Node.h"
+
+void Node::attachChild(NodePtr child)
+{
+	child->parent = this;
+	children.push_back(std::move(child));
+}
+
+Node::NodePtr Node::detachChild(const Node& node)
+{
+	int childFound = -1;
+	for(int i=0;i<children.size();i++)
+	{
+		if(children[i].get() == &node)
+		{
+			childFound = i;
+			break;
+		}
+	}
+
+	assert(childFound >= 0);
+
+	NodePtr returnNode = std::move(children[childFound]);
+	returnNode->parent = nullptr;
+	children.erase(children.begin() + childFound);
+	return returnNode;
+}
+
+void Node::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	// Two transforms are applied one after the other using the * operator
+
+	// This takes the parent's absolute transform and then applies the current node's
+	// relative transform. So this achieves the absolute world transform of the current node
+	// this is also how sf::Sprite handles it by combining it's own transform with the passed
+	// render state
+	states.transform *= getTransform();
+
+	// now that we have the absolute transform we can draw the node
+	drawCurrent(target,states);
+
+	// recursively draw all children
+	for(const NodePtr& child : children)
+	{
+		child->draw(target,states);
+	}
+}
+
+void Node::fixedUpdateChildren(const float dt)
+{
+	for(NodePtr &child : children)
+		child->fixedUpdate(dt);
+}
+
+void Node::updateChildren(const float dt)
+{
+	for(NodePtr &child : children)
+		child->update(dt);
+}
+
+void Node::fixedUpdate(const float dt)
+{
+	fixedUpateCurrent(dt);
+	fixedUpdateChildren(dt);
+} 
+
+void Node::update(const float dt)
+{
+	updateCurrent(dt);
+	updateChildren(dt);
+}
+
+sf::Transform Node::getWorldTransform() const
+{
+	sf::Transform transform = sf::Transform::Identity;
+
+	// Get absolute transform by accumulating all the transformations up the tree
+	// until we reach the root node. That will yield world transform ie. transform relative
+	// to root node. same thing
+	for(const Node* node = this; node != nullptr; node = node->parent)
+	{
+		transform = node->getTransform() * transform;
+	}
+	return transform;
+}
+
+sf::Vector2f Node::getWorldPosition() const
+{
+	// Apply transformation of world pos with origin
+	// to get position relative to origin ie world coords
+	return getWorldTransform() * sf::Vector2f();
+}
+
---
+++ Node.h
@@ -0,0 +1,48 @@
+//Copyright Sam Collier 2022
+
+//**************************************************************************************
+//Heavily influenced by 
+//Chapter 3 of SFML Game Development by Artur Moreira, Henrik V. Hansson, and Jan Haller
+//**************************************************************************************
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <cassert>
+
+#include <SFML/Graphics.hpp>
+
+// Nodes hold a ptr to their parent and smrt ptrs to all their children
+// this allows us to recursively traverse every entity in the game
+// Benefits: Coordinates relative to parent, broadcast to all nodes with ease
+class Node : public sf::Transformable, public sf::Drawable, private sf::NonCopyable
+{
+	public:
+		typedef std::unique_ptr<Node> NodePtr;
+		void attachChild(NodePtr child);
+		NodePtr detachChild(const Node& node);
+
+		void fixedUpdate(const float dt);
+		void update(const float dt);
+		sf::Transform getWorldTransform() const;
+		sf::Vector2f getWorldPosition() const;
+		virtual unsigned int getCategory() const;
+
+	private:
+		std::vector<NodePtr> children;
+		Node* parent = nullptr;
+
+		// final means that children of this class cannot override it
+		// draw renders the entire node and children
+		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override final;
+
+		// only draws the current node but not it's children
+		virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const{};
+
+		// children are expected to override their respective 'current' methods
+		virtual void fixedUpateCurrent(const float dt){};
+		void fixedUpdateChildren(const float dt);
+		virtual void updateCurrent(const float dt){};
+		void updateChildren(const float dt);
+};
