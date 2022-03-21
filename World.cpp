871f04471e3fb77554eb873e9e103b6c654b3e2c
Sam Collier <sam@samcollier.tech>
2022-03-21 02:21:49 +0000

    Tiled collision object layer

--- World.cpp
+++ World.cpp
@@ -84,32 +84,49 @@ void World::loadFromFile(const char* fileName, TextureHolder& textures)
 
 	for(auto pair : tileSets)
 	{
-		textures.load(textureID,pair.first);
+		textures.load(textureID++,pair.first);
 	}
 
 	for(json layer : layers)
 	{
-		std::vector<int> layout = layer["data"];
-		int i = 0;
-		for(int y = 0; y<mapHeight; y++)
+		if(!layer["data"].is_null())
 		{
-			for(int x = 0; x<mapWidth; x++)
+			std::vector<int> layout = layer["data"];
+			int i = 0;
+			for(int y = 0; y<mapHeight; y++)
 			{
-				if(layout[i] != 0)
+				for(int x = 0; x<mapWidth; x++)
 				{
-					std::unique_ptr<Actor> tile(new Actor(textures));
-					tile->setTexture(0);
-					tile->setPosition(x*tileWidth,y*tileHeight);
-					sf::IntRect texRect = sf::IntRect(
-								(layout[i]-1)*tileWidth,0,
-								tileWidth,tileHeight);
-					tile->setTextureRect(texRect);
-					tile->setCollisionBox(sf::FloatRect(0,0,tileWidth,tileHeight));
-					addNode(&tile,Object,true);
+					if(layout[i] != 0)
+					{
+						std::unique_ptr<Actor> tile(new Actor(textures));
+						tile->setTexture(textureID-1);
+						tile->setPosition(x*tileWidth,y*tileHeight);
+						sf::IntRect texRect = sf::IntRect(
+									(layout[i]-1)*tileWidth,0,
+									tileWidth,tileHeight);
+						tile->setTextureRect(texRect);
+						addNode(&tile,Object,false);
+					}
+					i++;
 				}
-				i++;
 			}
 		}
+		if(!layer["objects"].is_null())
+		{
+			for(json object : layer["objects"])
+			{
+				sf::FloatRect rect = sf::FloatRect(object["x"],object["y"],object["width"],object["height"]);
+				if(object["properties"].at(0)["value"] == "block")
+				{
+					std::unique_ptr<Actor> colBox(new Actor(textures));
+					colBox->setPosition(rect.left,rect.top);
+					colBox->setCollisionBox(sf::FloatRect(0.f,0.f,rect.width,rect.height));
+					addNode(&colBox,Object,true);
+				}
+			}
+		}
+		
 	}
 }
 
